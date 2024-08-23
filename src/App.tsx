3de3c544f2d26b228a8aee9988ddab7bf7d9bd01
jaeyonging <wodyd1318@naver.com>
2024-08-23 14:57:28 +0900

    delete games

--- src/App.tsx
+++ src/App.tsx
@@ -8,28 +8,7 @@ function App() {
     <Suspense fallback={<div>로딩중</div>}>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route
-          path="/games/cooking"
-          element={
-            <iframe
-              src="/games/cooking/index.html"
-              title="cooking"
-              style={{ width: '100%', height: '100vh', border: 'none' }}
-            />
-          }
-        />
-        <Route
-          path="/games/racing"
-          element={
-            <iframe
-              src="/games/racing/index.html"
-              title="racing"
-              style={{ width: '100%', height: '100vh', border: 'none' }}
-            />
-          }
-        />
       </Routes>
-
     </Suspense>
   );
 }
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
 
 export const Home = () => {
-  const navigate = useNavigate()
+  const openUrl = (url: string) => {
+    window.open(url, '_blank')
+  }
+
   return (
     <div className='flex w-[100vw] text-[red] h-[100vh] justify-center items-center'>
-      <div className='w-[100px] text-[white] bg-[black]' onClick={() => navigate("/games/cooking")}>
+      <div
+        className='w-[100px] text-[white] bg-[black] cursor-pointer'
+        onClick={() => openUrl("https://html-classic.itch.zone/html/9152267/dist/index.html")}
+      >
         Button
       </div>
     </div>
