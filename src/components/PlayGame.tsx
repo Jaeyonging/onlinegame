701883141984a322186ad3665df0792ef556ec23
jaeyonging <wodyd1318@naver.com>
2024-08-23 15:32:05 +0900

    add game again

--- src/components/PlayGame.tsx
+++ src/components/PlayGame.tsx
@@ -2,13 +2,12 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import JSZip from 'jszip';
 
-export const PlayGame: React.FC = () => {
+export const PlayGame = () => {
     const { name } = useParams<{ name: string }>();
-
     return (
         <iframe
             src={`/games/${name}/index.html`}
-            title="cooking"
+            title="Game 01"
             style={{ width: '100%', height: '100vh', border: 'none' }}
         />
     );
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,15 +1,14 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export const Home = () => {
-  const openUrl = (url: string) => {
-    window.location.href = (url)
-  }
+  const navigate = useNavigate()
 
   return (
     <div className='flex w-[100vw] text-[red] h-[100vh] justify-center items-center'>
       <div
         className='w-[100px] text-[white] bg-[black] cursor-pointer'
-        onClick={() => openUrl("https://html-classic.itch.zone/html/9152267/dist/index.html")}
+        onClick={() => navigate("/games/cooking")}
       >
         Button
       </div>
