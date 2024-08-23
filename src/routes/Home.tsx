3430fe37752636290f88662912290db2793b57b2
jaeyonging <wodyd1318@naver.com>
2024-08-23 18:29:33 +0900

    add games

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -9,13 +9,49 @@ export const Home = () => {
     <div className='flex w-[100vw] text-[red] h-[100vh] justify-center items-center'>
       <div
         className='w-[100px] text-[white] bg-[black] cursor-pointer'
+        onClick={() => navigate(`/games/bird?dan=${dan}`)}
+      >
+        Button
+      </div>
+      <div
+        className='w-[100px] text-[white] bg-[black] cursor-pointer'
         onClick={() => navigate(`/games/cooking?dan=${dan}`)}
       >
         Button
       </div>
       <div
         className='w-[100px] text-[white] bg-[black] cursor-pointer'
+        onClick={() => navigate(`/games/earthprotect?dan=${dan}`)}
+      >
+        Button
+      </div>
+      <div
+        className='w-[100px] text-[white] bg-[black] cursor-pointer'
+        onClick={() => navigate(`/games/fortress?dan=${dan}`)}
+      >
+        Button
+      </div>
+      <div
+        className='w-[100px] text-[white] bg-[black] cursor-pointer'
+        onClick={() => navigate(`/games/hangle?dan=${dan}`)}
+      >
+        Button
+      </div>
+      <div
+        className='w-[100px] text-[white] bg-[black] cursor-pointer'
         onClick={() => navigate(`/games/racing?dan=${dan}`)}
+      >
+        Button
+      </div>
+      <div
+        className='w-[100px] text-[white] bg-[black] cursor-pointer'
+        onClick={() => navigate(`/games/shootingGame?dan=${dan}`)}
+      >
+        Button
+      </div>
+      <div
+        className='w-[100px] text-[white] bg-[black] cursor-pointer'
+        onClick={() => navigate(`/games/waterTank?dan=${dan}`)}
       >
         Button
       </div>
