2c4ae2e749ef45a27bec8cf3b23a7b1dbb7dcde2
jaeyonging <wodyd1318@naver.com>
2024-08-23 14:49:22 +0900

    racing added

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -5,7 +5,7 @@ export const Home = () => {
   const navigate = useNavigate()
   return (
     <div className='flex w-[100vw] text-[red] h-[100vh] justify-center items-center'>
-      <div className='w-[100px] text-[white] bg-[black]' onClick={() => navigate("/games/01")}>
+      <div className='w-[100px] text-[white] bg-[black]' onClick={() => navigate("/games/cooking")}>
         Button
       </div>
     </div>
